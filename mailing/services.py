e74b0c0f1e668488ac20c099a299ee201e92cd31
va0r <i@vialor.ru>
2023-09-06 16:21:55 +0300

    (va0r)

--- mailing/services.py
+++ mailing/services.py
@@ -26,13 +26,9 @@ def send_mail_all():
     now = datetime.datetime.now()
     now_utc = now.astimezone(datetime.timezone.utc)
     for ms in MailingSettings.objects.filter(status=MailingSettings.STATUS_STARTED):
-        print(f'{ms.groups.all() = }')
         for mg in ms.groups.all():
-            print(f'{mg = }')
             group_object = ClientGroup.objects.get(name=mg)
-            print(f'{group_object = }')
             clients_in_group = Client.objects.filter(groups=group_object)
-            print(f'{clients_in_group = }')
             if not clients_in_group.exists():
                 return
             for mc in clients_in_group.all():
@@ -42,16 +38,12 @@ def send_mail_all():
                     last_try_date_utc = last_try_date.last_try.astimezone(datetime.timezone.utc)
                     if ms.period == MailingSettings.PERIOD_DAILY:
                         if (now_utc - last_try_date_utc).days >= 1:
-                            # print('daily')
                             send_email_one(ms, mc)
                     elif ms.period == MailingSettings.PERIOD_WEEKLY:
                         if (now_utc - last_try_date_utc).days >= 7:
-                            # print('weekly')
                             send_email_one(ms, mc)
                     elif ms.period == MailingSettings.PERIOD_MONTHLY:
                         if (now_utc - last_try_date_utc).days >= 30:
-                            # print('monthly')
                             send_email_one(ms, mc)
                     else:
-                        # print('else')
                         send_email_one(ms, mc)

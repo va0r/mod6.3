3c21ea1c3e401f56db80e04f770dd1f140f8deea
va0r <i@vialor.ru>
2023-09-06 22:57:39 +0300

    (va0r)

--- logging_config.py
+++ logging_config.py
@@ -4,8 +4,8 @@ import logging
 log_format = '%(asctime)s - %(name)s - %(levelname)s - %(message)s'
 
 # Определите настройки журналирования
-logging.basicConfig(level=logging.INFO, format=log_format)
+logging.basicConfig(level=logging.WARNING, format=log_format)
 
 # Подключитесь к журналу Celery
 celery_logger = logging.getLogger('celery')
-celery_logger.setLevel(logging.INFO)
+celery_logger.setLevel(logging.WARNING)
